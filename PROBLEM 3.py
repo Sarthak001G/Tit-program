10b519e2ba6f765d96b7ce39acc53b8bec298b90
Sarthak001G <sarthakgautam60@gmail.com>
2024-07-04 22:50:59 +0530

    Create PROBLEM 3 .py
    Here's a Python program that uses the os module to list the contents of a directory:

---
+++ PROBLEM 3.py
@@ -0,0 +1,16 @@
+import os
+
+def list_directory_contents(path='.'):
+    try:
+        # Get the list of all files and directories
+        dir_list = os.listdir(path)
+        print(f"Contents of '{path}':")
+        for item in dir_list:
+            print(item)
+    except FileNotFoundError:
+        print(f"The directory '{path}' does not exist.")
+    except PermissionError:
+        print(f"Permission denied to access '{path}'.")
+
+# Example usage
+list_directory_contents=input('/TIT PROGRAM')
