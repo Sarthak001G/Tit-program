36c10e7ebaca68e27b2acf569effc37596c52ef5
Sarthak001G <sarthakgautam60@gmail.com>
2024-07-11 23:49:24 +0530

    Create P_6.py
    Que:-Write aprogram that finds an element's index in a tuple WITHOUT using index()?

---
+++ P_6.py
@@ -0,0 +1,10 @@
+# Que:-Write aprogram that finds an element's index in a tuple WITHOUT using index()?
+tuple1 = ('a','p','p','l','e')
+char = input ("Enter a single letter without quotes:")
+if char in tuple1 :
+    count = 0
+    for a in tuple1 :
+        if a != char :
+            count +=1
+        else : print (char, "is NOT in ", tuple1)
+           
